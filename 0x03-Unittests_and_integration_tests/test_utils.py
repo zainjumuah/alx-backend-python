7ed3c41beeb5509438f86ee92380c1696f8a6e16
zainjumuah <your.email@example.com~>
2025-11-21 16:10:49 +0100

    spent a whole 4 haars on this task. Phew

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+"""
+Example inputs (from the intranet o):
+nested_map={"a": 1}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a", "b")
+"""
+
+#Guys, for each of the input above, I'm supposed to test with assertEqual that the functions returns the expected result
+#Oh and, the body of the test method shouldbe no longer than 2 lines
+
