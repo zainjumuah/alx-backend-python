458339d8b6dafde2de2eba2a8b9d7d18a79f86bc
zainjumuah <your.email@example.com~>
2025-11-22 15:48:43 +0100

    I spent 10 million years on this

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/home/zain/ALX/ALX_env/bin/python
 
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+"""
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -13,13 +14,23 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-"""
+
 Example inputs (from the intranet o):
 nested_map={"a": 1}, path=("a",)
 nested_map={"a": {"b": 2}}, path=("a",)
 nested_map={"a": {"b": 2}}, path=("a", "b")
-"""
 
 #Guys, for each of the input above, I'm supposed to test with assertEqual that the functions returns the expected result
 #Oh and, the body of the test method shouldbe no longer than 2 lines
+"""
+#Task 2
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
 
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        with self.assertRaises(KeyError) as ctx:
+            access_nested_map(nested_map, path)
+        self.assertEqual(ctx.exception.args[0], expected)
