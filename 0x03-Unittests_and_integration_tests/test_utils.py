47a9269efc445ab3300bbaa1dfd20719eaf56385
zainjumuah <your.email@example.com~>
2025-11-23 19:23:36 +0100

    please, Checker

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,36 +1,88 @@
 #!/usr/bin/env python3
-
+"""
+Parameterize a unit test
+"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
 
-"""
+
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    class that inherits from unittest.TestCase
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """
+        method to test that the method returns what it is supposed to.
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-
-Example inputs (from the intranet o):
-nested_map={"a": 1}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a", "b")
-
-#Guys, for each of the input above, I'm supposed to test with assertEqual that the functions returns the expected result
-#Oh and, the body of the test method shouldbe no longer than 2 lines
-"""
-#Task 2
-class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
-
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        with self.assertRaises(KeyError) as ctx:
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+         test that a KeyError is raised for
+         the following inputs (use @parameterized.expand)
+        """
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-        self.assertEqual(ctx.exception.args[0], expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+     implement the TestGetJson.test_get_json
+     method to test that utils.get_json returns the expected result.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """
+        method to test that the method returns what it is supposed to.
+        """
+        class Mocked(Mock):
+            """
+            class that inherits from Mock
+            """
+
+            def json(self):
+                """
+                json returning a payload
+                """
+                return payload
+
+        with patch("requests.get") as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(unittest.TestCase):
+    ''' memoize unittest '''
+
+    def test_memoize(self):
+        ''' memoize test '''
+
+        class TestClass:
+            ''' self descriptive'''
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mocked:
+            spec = TestClass()
+            spec.a_property
+            spec.a_property
+            mocked.asset_called_once()
